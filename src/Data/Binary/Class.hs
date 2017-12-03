36b62a72bf4f19f4c6a814f41f6edc8a8e174268
Michal Terepeta <michal.terepeta@gmail.com>
2017-12-03 16:36:57 +0100

    Support Int8Rep and Word8Rep
    Signed-off-by: Michal Terepeta <michal.terepeta@gmail.com>

--- src/Data/Binary/Class.hs
+++ src/Data/Binary/Class.hs
@@ -873,6 +873,10 @@ instance Binary RuntimeRep where
     put AddrRep         = putWord8 9
     put FloatRep        = putWord8 10
     put DoubleRep       = putWord8 11
+#if __GLASGOW_HASKELL__ >= 803
+    put Int8Rep          = putWord8 12
+    put Word8Rep         = putWord8 13
+#endif
 
     get = do
         tag <- getWord8
@@ -889,6 +893,10 @@ instance Binary RuntimeRep where
           9  -> pure AddrRep
           10 -> pure FloatRep
           11 -> pure DoubleRep
+#if __GLASGOW_HASKELL__ >= 803
+          12 -> pure Int8Rep
+          13 -> pure Word8Rep
+#endif
           _  -> fail "GHCi.TH.Binary.putRuntimeRep: invalid tag"
 
 -- | @since 0.8.5.0. See #typeable-instances#
